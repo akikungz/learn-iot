e2e0fd238039f14df53fe195ed450023d5da5d73
Thitipong Tapianthong <akikungz@proton.me>
2024-07-12 10:15:17 +0700

    Refactor main.cpp for week 1

--- src/week-1/main.cpp
+++ src/week-1/main.cpp
@@ -1,24 +1,24 @@
 #include <Arduino.h>
 
-// const int analogInPin = A0;
+const int analogInPin = A0;
 
-// void setup() {
-//   Serial.begin(9600);
+void setup() {
+  Serial.begin(9600);
 
-//   pinMode(D4, OUTPUT);
-// }
+  pinMode(D4, OUTPUT);
+}
 
-// void loop() {
-//   int out = analogRead(analogInPin);
+void loop() {
+  int out = analogRead(analogInPin);
 
-//   Serial.print("Analog input = ");
-//   Serial.println(out);
+  Serial.print("Analog input = ");
+  Serial.println(out);
 
-//   if (out > 500) {
-//     digitalWrite(D4, HIGH);
-//   } else {
-//     digitalWrite(D4, LOW);
-//   }
+  if (out > 500) {
+    digitalWrite(D4, HIGH);
+  } else {
+    digitalWrite(D4, LOW);
+  }
 
-//   delay(10);
-// }
+  delay(10);
+}
