3b9c07114c39722d641345e139150e5850d5c7d3
Thitipong Tapianthong <akikungz@proton.me>
2024-07-19 09:03:25 +0700

    edit: add comment to src/**/*.cpp

--- src/week-2/analog_test.cpp
+++ src/week-2/analog_test.cpp
@@ -1,29 +1,29 @@
-#include <Arduino.h>
+// #include <Arduino.h>
 
-void setup () {
-    Serial.begin(9600);
+// void setup () {
+//     Serial.begin(9600);
 
-    pinMode(A0, INPUT);
-    pinMode(2, OUTPUT);
+//     pinMode(A0, INPUT);
+//     pinMode(2, OUTPUT);
 
-    pinMode(8, OUTPUT);
-    pinMode(9, OUTPUT);
+//     pinMode(8, OUTPUT);
+//     pinMode(9, OUTPUT);
 
-    digitalWrite(8, LOW);
-    digitalWrite(9, LOW);
-}
+//     digitalWrite(8, LOW);
+//     digitalWrite(9, LOW);
+// }
 
-void loop () {
-    int sensorValue = analogRead(A0);
-    int ledValue = map(sensorValue, 0, 1023, 0, 255);
+// void loop () {
+//     int sensorValue = analogRead(A0);
+//     int ledValue = map(sensorValue, 0, 1023, 0, 255);
 
-    analogWrite(2, ledValue);
+//     analogWrite(2, ledValue);
 
-    Serial.print("sensorValue = ");
-    Serial.print(sensorValue);
-    Serial.print("\t");
-    Serial.print("ledValue = ");
-    Serial.println(ledValue);
+//     Serial.print("sensorValue = ");
+//     Serial.print(sensorValue);
+//     Serial.print("\t");
+//     Serial.print("ledValue = ");
+//     Serial.println(ledValue);
 
-    delay(500);
-}
+//     delay(500);
+// }
--- src/week-2/dht22_test.cpp
+++ src/week-2/dht22_test.cpp
@@ -1,62 +1,62 @@
-#include <Arduino.h>
-#include <DHT.h>
+// #include <Arduino.h>
+// #include <DHT.h>
 
-#define DHTPIN 13
+// #define DHTPIN 13
 
-const int LED_GREEN = 2;
-const int LED_YELLOW = 14;
-const int LED_RED = 12;
+// const int LED_GREEN = 2;
+// const int LED_YELLOW = 14;
+// const int LED_RED = 12;
 
-DHT dht(DHTPIN, DHT22);
+// DHT dht(DHTPIN, DHT22);
 
-float temp = 0;
+// float temp = 0;
 
-void setup() {
-    Serial.begin(9600);
+// void setup() {
+//     Serial.begin(9600);
 
-    dht.begin();
+//     dht.begin();
 
-    pinMode(LED_GREEN, OUTPUT);
-    pinMode(LED_YELLOW, OUTPUT);
-    pinMode(LED_RED, OUTPUT);
-}
+//     pinMode(LED_GREEN, OUTPUT);
+//     pinMode(LED_YELLOW, OUTPUT);
+//     pinMode(LED_RED, OUTPUT);
+// }
 
-void loop () {
-    float t = dht.readTemperature();
+// void loop () {
+//     float t = dht.readTemperature();
 
-    if (isnan(t)) {
-        Serial.println("Failed to read from DHT sensor!");
-        return;
-    }
+//     if (isnan(t)) {
+//         Serial.println("Failed to read from DHT sensor!");
+//         return;
+//     }
 
-    if (temp == 0) {
-        temp = t;
-    } else {
-        Serial.print("Before: ");
-        Serial.println(temp);
+//     if (temp == 0) {
+//         temp = t;
+//     } else {
+//         Serial.print("Before: ");
+//         Serial.println(temp);
 
-        Serial.print("After: ");
-        Serial.println(t);
+//         Serial.print("After: ");
+//         Serial.println(t);
 
-        if (t > temp) {
-            digitalWrite(LED_GREEN, HIGH);
-            delay(1000);
-            digitalWrite(LED_YELLOW, HIGH);
-            delay(1000);
-            digitalWrite(LED_RED, HIGH);
-        } else if (t < temp) {
-            digitalWrite(LED_RED, HIGH);
-            delay(1000);
-            digitalWrite(LED_YELLOW, HIGH);
-            delay(1000);
-            digitalWrite(LED_GREEN, HIGH);
-        }
-    }
+//         if (t > temp) {
+//             digitalWrite(LED_GREEN, HIGH);
+//             delay(1000);
+//             digitalWrite(LED_YELLOW, HIGH);
+//             delay(1000);
+//             digitalWrite(LED_RED, HIGH);
+//         } else if (t < temp) {
+//             digitalWrite(LED_RED, HIGH);
+//             delay(1000);
+//             digitalWrite(LED_YELLOW, HIGH);
+//             delay(1000);
+//             digitalWrite(LED_GREEN, HIGH);
+//         }
+//     }
 
-    delay(2000);
-    digitalWrite(LED_GREEN, LOW);
-    digitalWrite(LED_YELLOW, LOW);
-    digitalWrite(LED_RED, LOW);
+//     delay(2000);
+//     digitalWrite(LED_GREEN, LOW);
+//     digitalWrite(LED_YELLOW, LOW);
+//     digitalWrite(LED_RED, LOW);
 
-    temp = t;
-}
+//     temp = t;
+// }
